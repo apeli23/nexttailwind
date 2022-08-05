922d60d65c4b481985c830f5942850bf1615fd36
apeli23 <apelibrian@gmail.com>
2022-08-05 12:07:57 +0300

    create-login-API

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import '../styles/globals.css';
 import { StoreProvider } from '../utils/Store';
+import { SessionProvider } from 'next-auth/react';
 
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
+    <SessionProvider session={session}>
     <StoreProvider>
       <Component {...pageProps} />
     </StoreProvider>
+  </SessionProvider>
   );
 }
 
